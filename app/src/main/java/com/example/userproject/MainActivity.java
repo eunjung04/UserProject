79ec50664aba06b1e58bb61d3ccd9906c3948dd3
peunj04 <peunj04@gmail.com>
2019-11-16 14:02:44 +0900

    Toast message 추가.

--- app/src/main/java/com/example/userproject/MainActivity.java
+++ app/src/main/java/com/example/userproject/MainActivity.java
@@ -3,12 +3,38 @@ package com.example.userproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toast toast;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.main_button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showToast("toast message");
+            }
+        });
+
+
     }
+
+    private void showToast(String message) {
+        if ( toast == null){
+            toast = Toast.makeText(this, message, Toast.LENGTH_SHORT);
+        }else {
+            toast.setText(message);
+        }
+        toast.show();
+    }
+
 }
